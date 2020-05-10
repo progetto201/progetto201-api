1d1a4a1296fa5d22319a79f6226569c81561b4cb
Stefano Zenaro <mariortgasd@hotmail.com>
2020-05-10 21:32:26 +0200

    tentativo gh-actions

---
+++ tests/test.py
@@ -0,0 +1,66 @@
+import unittest
+import requests
+import json
+import testcases_date as tc_date
+
+
+class TestApi(unittest.TestCase):
+
+    def test_date(self):
+        """
+        Tests the /api/options/date.php script.
+
+        A loop tests each test case.
+        Each test case has a request method, headers and a payload.
+
+        The response is always in JSON format, so if json.loads()
+        fails, it means there is a problem with the PHP code and the test will fail 
+
+        The response should be the expected one... if not the test will fail
+
+        The status code should be the expected one if there's an error or 200 if there's no error.
+        Otherwise the test will fail
+        """
+        # date.php url
+        url = "http://localhost/api/options/date.php"
+
+        # loop for each test case
+        for testcase in tc_date.testcases_date:
+            # request the url with the test case method, headers and payload
+            response = requests.request(testcase["method"], url, headers=testcase["headers"], data = testcase["payload"])
+            
+            # try to convert the response in json data
+            json_data = json.loads(response.text)
+
+            # check the expected output type: if it is a dictionary the expected result is one,
+            # otherwise the output could be one of the many expected results
+            if type(testcase["expected_output"]) == dict:
+                # the json data should correspond to the expected result
+                self.assertEqual(json_data, testcase["expected_output"])
+            else:
+                # check if one of the expected result is equal to the response
+                check = False
+
+                # loop for each possible output
+                for expected_output in testcase["expected_output"]:
+                    if json_data == expected_output:
+                        # the output is equal to one of the possible outputs,
+                        # the check has been successfull
+                        check = True
+                        break
+                
+                # fail test if check is False
+                self.assertTrue(check)
+            
+            # if there are no errors in the reponse, the status code should be
+            # the "htmlcode" of the first error.
+            # No errors = status 200 OK
+            if len(json_data["errors"]) > 0:
+                self.assertEqual(response.status_code, json_data["errors"][0]["htmlcode"])
+            else:
+                self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+
+    unittest.main()
---
+++ tests/testcases_date.py
@@ -0,0 +1,179 @@
+testcases_date = [
+    # wrong request method
+    {
+        "method": "GET",
+        "payload": {}, 
+        "headers": {}, 
+        "expected_output": {
+            "errors": [
+                {
+                "id": 800,
+                "htmlcode": 405,
+                "message": "Method not supported by the target resource"
+                }
+            ]
+        }
+    },
+
+    # correct request method, no data
+    {
+        "method": "POST",
+        "payload": {}, 
+        "headers": {}, 
+        "expected_output": {
+            "errors": [
+                {
+                "id": 813,
+                "htmlcode": 400,
+                "message": "'mintime' and/or 'maxtime' are/is not set in the request"
+                }
+            ]
+        }
+    },
+
+    # correct request method, no maxtime data
+    {
+        "method": "POST",
+        "payload": {'mintime': '20'}, 
+        "headers": {}, 
+        "expected_output": {
+            "errors": [
+                {
+                "id": 813,
+                "htmlcode": 400,
+                "message": "'mintime' and/or 'maxtime' are/is not set in the request"
+                }
+            ]
+        }
+    },
+
+    # correct request method, no mintime data
+    {
+        "method": "POST",
+        "payload": {'maxtime': '20'}, 
+        "headers": {}, 
+        "expected_output": {
+            "errors": [
+                {
+                "id": 813,
+                "htmlcode": 400,
+                "message": "'mintime' and/or 'maxtime' are/is not set in the request"
+                }
+            ]
+        }
+    },
+
+    # correct request method, mintime > maxtime
+    {
+        "method": "POST",
+        "payload": {'maxtime': '20', 'mintime': '50'}, 
+        "headers": {}, 
+        "expected_output": {
+            "errors": [
+                {
+                "id": 810,
+                "htmlcode": 422,
+                "message": "'from' date should come before the 'to' date"
+                }
+            ]
+        }
+    },
+
+    # correct request method, mintime == maxtime ( != 0 )
+    {
+        "method": "POST",
+        "payload": {'maxtime': '50', 'mintime': '50'}, 
+        "headers": {}, 
+        "expected_output": {
+            "errors": [
+                {
+                "id": 811,
+                "htmlcode": 422,
+                "message": "'from' date should be different to the 'to' date (or both zero)"
+                }
+            ]
+        }
+    },
+
+    # correct request type, mintime and maxtime (mintime not numeric)
+    {
+        "method": "POST",
+        "payload": {'maxtime': '30', 'mintime': 'test'}, 
+        "headers": {}, 
+        "expected_output": {
+            "errors": [
+                {
+                "id": 812,
+                "htmlcode": 422,
+                "message": "'from' date and/or 'to' date should be numeric"
+                }
+            ]
+        }
+    },
+
+    # correct request type, mintime and maxtime (maxtime not numeric)
+    {
+        "method": "POST",
+        "payload": {'maxtime': 'test', 'mintime': '30'}, 
+        "headers": {}, 
+        "expected_output": {
+            "errors": [
+                {
+                "id": 812,
+                "htmlcode": 422,
+                "message": "'from' date and/or 'to' date should be numeric"
+                }
+            ]
+        }
+    },
+
+    # correct request type, mintime and maxtime set correctly (both to zero)
+    {
+        "method": "POST",
+        "payload": {'maxtime': '0', 'mintime': '0'}, 
+        "headers": {}, 
+        "expected_output": [
+            {
+                "errors": [
+                    {
+                        "id": 820,
+                        "htmlcode": 500,
+                        "message": "Unable to change 'from' date and 'to' date (maybe values are already set?)"
+                    }
+                ]
+            },
+            {
+                "errors": [],
+                "data": {
+                    "mintime": 0,
+                    "maxtime": 0
+                }
+            }
+        ]
+    },
+
+    # correct request type, mintime and maxtime set correctly (not zero)
+    {
+        "method": "POST",
+        "payload": {'maxtime': '100', 'mintime': '40'}, 
+        "headers": {}, 
+        "expected_output": [
+            {
+                "errors": [
+                    {
+                        "id": 820,
+                        "htmlcode": 500,
+                        "message": "Unable to change 'from' date and 'to' date (maybe values are already set?)"
+                    }
+                ]
+            },
+            {
+                "errors": [],
+                "data": {
+                    "mintime": 40,
+                    "maxtime": 100
+                }
+            }
+        ]
+    },
+]
